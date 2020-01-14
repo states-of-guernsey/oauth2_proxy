1099c61a418ce64c95a498f273d2fd69b60a9eae
Duarte Mechas <dmechas@equalexperts.com>
2020-01-14 14:01:46 +0000

    CX-1408 build task script to tar the binary

--- ci/tasks/scripts/build.sh
+++ ci/tasks/scripts/build.sh
@@ -2,6 +2,7 @@
 
 set -e -u
 
+BASE_DIR=$PWD
 BINARY="oauth2_proxy"
 
 VERSION="$(shell git describe --always --dirty --tags 2>/dev/null || echo '0.0.0-dev')"
@@ -12,16 +13,18 @@ fi
 mkdir -p $GOPATH/src/github.com/pusher
 cp -R oauth2_proxy $GOPATH/src/github.com/pusher/oauth2_proxy
 
+ASSETS_DIR="$BASE_DIR/assets/"
+OUTPUT_PATH="$ASSETS_DIR/$BINARY-$VERSION.linux-amd64"
 
-mkdir assets/$BINARY-$VERSION.linux-amd64
+mkdir $OUTPUT_PATH
 
 cd $GOPATH/src/github.com/pusher/oauth2_proxy
 
 GO111MODULE=on GOOS=linux GOARCH=amd64 go build -ldflags="-X main.VERSION=$VERSION" \
-	-o assets/$BINARY-$VERSION.linux-amd64/$BINARY github.com/pusher/oauth2_proxy
+  -o "$OUTPUT_PATH/$BINARY" github.com/pusher/oauth2_proxy
 
-shasum -a 256 assets/$BINARY-$VERSION.linux-amd64/$BINARY > assets/$BINARY-$VERSION.linux-amd64-sha256sum.txt
-tar -C assets -czvf assets/$BINARY-$VERSION.linux-amd64.tar.gz $BINARY-$VERSION.linux-amd64
+shasum -a 256 $OUTPUT_PATH/$BINARY >$ASSETS_DIR/$BINARY-$VERSION.linux-amd64-sha256sum.txt
+tar -C $ASSETS_DIR -czvf $ASSETS_DIR/$BINARY-$VERSION.linux-amd64.tar.gz $BINARY-$VERSION.linux-amd64
 
 echo "printing VERSION..."
-./assets/$BINARY-$VERSION.linux-amd64/$BINARY -version
+$OUTPUT_PATH/$BINARY -version
